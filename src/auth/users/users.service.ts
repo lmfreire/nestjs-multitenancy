2e55b4cdbb6b36206f3b9ec1e29eba585bc5e213
lmFreire <l.matheus.freire10@gmail.com>
2024-11-21 22:45:40 -0300

    confiração prisma module

--- src/auth/users/users.service.ts
+++ src/auth/users/users.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
 
-    create(data){
+    constructor(private prismaService: PrismaService){}
 
+    create(data){
+        
     }
 }
