37f285f7e2d9e3c66244d6e5adb752323174e22a
lmFreire <l.matheus.freire10@gmail.com>
2024-11-23 12:12:03 -0300

    create user partner

--- src/auth/users/users.service.ts
+++ src/auth/users/users.service.ts
@@ -9,12 +9,22 @@ export class UsersService {
 
     constructor(private prismaService: PrismaService){}
 
-    create(data: CreateUserDto){
+    createPartnerUser(data: CreateUserDto){
         return this.prismaService.user.create({
             data: {
                 ...data,
                 password: this.generateHash(data.password),
                 roles: [UserRoles.PARTNER]
+            }
+        })
+    }
+
+    createCommonUser(data: CreateUserDto){
+        return this.prismaService.user.create({
+            data: {
+                ...data,
+                password: this.generateHash(data.password),
+                roles: [UserRoles.USER]
             }
         })
     }
